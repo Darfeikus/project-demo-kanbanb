5835858bf53bfb2beb7a4ecc75649ee72eed608d
Darfeikus <racso_boston@hotmail.com>
2020-09-02 11:02:20 -0500

    Code is ok #1

--- addition.java
+++ addition.java
@@ -1,5 +1,11 @@
+/*
+	Author: darfeikus
+	Date: Sep 2, 2020
+*/
 public class addition{
 	public static void main(String args[]){
 		System.out.println("I'm Addition");
+		System.out.println("This is another commit");
 	}
 }
+
