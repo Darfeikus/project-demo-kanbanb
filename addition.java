0f0d1c1222de318098661a8e4a24a77cf1b17ed1
Darfeikus <racso_boston@hotmail.com>
2020-09-02 11:17:06 -0500

    Ya arregle mi codigo feo #1

--- addition.java
+++ addition.java
@@ -2,10 +2,7 @@
 	Author: darfeikus
 	Date: Sep 2, 2020
 */
-public class addition{
-	public static void main(String args[]){
-		System.out.println("I'm Addition");
-		System.out.println("This is another commit");
-	}
-}
+/*
+	Ya lo arregle
+*/
 
